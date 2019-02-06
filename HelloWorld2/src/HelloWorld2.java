d774845c1b476322d31615cccf3d47cded828575
andrewreny <46968167+andrewreny@users.noreply.github.com>
2019-02-06 10:38:52 -0500

    Update HelloWorld2.java

--- HelloWorld2/src/HelloWorld2.java
+++ HelloWorld2/src/HelloWorld2.java
@@ -3,5 +3,6 @@ public class HelloWorld2 {
 	public static void main(String [] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hi Alice!");
+		System.out.println("Hi Charlie!");
 	}
 }
